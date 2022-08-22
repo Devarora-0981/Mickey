3cdc582e4abde6279c5597ac9f8b233cbbd705e6
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-08-23 00:16:32 +0530

    Update main.py

--- main.py
+++ main.py
@@ -30,7 +30,7 @@ async def is_admins(chat_id: int):
     ]
 
 
-@bot.on_message(filters.command([("cstart", "aistart")])
+@bot.on_message(filters.command(["cstart", "aistart"]))
 async def start(client, message):
         await message.reply_text("Hi! My name is Dev. I'm an Artificial Intelligence\n /aichatbot - [on|off]")
 
