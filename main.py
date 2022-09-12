03400094e56721eb8e298a8e16fcfb402701723e
Dev Arora <107177633+Devarora0981@users.noreply.github.com>
2022-09-12 21:30:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ API_ID = os.environ.get("API_ID", None)
 API_HASH = os.environ.get("API_HASH", None) 
 BOT_TOKEN = os.environ.get("BOT_TOKEN", None) 
 MONGO_URL = os.environ.get("MONGO_URL", None)
+GET_ME = os.environ.get("GET_ME", None) 
 
 
 bot = Client(
