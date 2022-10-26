8b3f9b8d1032f7fd5cd2b0fc4d9494e8cc6999b6
Dev Arora <115332419+Devarora-0981@users.noreply.github.com>
2022-10-26 21:55:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,26 +22,25 @@ OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
 SUPPORT_GRP = os.environ.get("SUPPORT_GRP")
 BOT_NAME = os.environ.get("BOT_NAME")
 START_IMG1 = os.environ.get("START_IMG1")
-START_IMG2 = os.environ.get("START_IMG2", None)
-START_IMG3 = os.environ.get("START_IMG3", None)
-START_IMG4 = os.environ.get("START_IMG4", None)
-START_IMG5 = os.environ.get("START_IMG5", None)
-START_IMG6 = os.environ.get("START_IMG6", None)
-START_IMG7 = os.environ.get("START_IMG7", None)
-START_IMG8 = os.environ.get("START_IMG8", None)
-START_IMG9 = os.environ.get("START_IMG9", None)
-START_IMG10 = os.environ.get("START_IMG10", None)
-STKR = os.environ.get("STKR")
-STKR1 = os.environ.get("STKR1", None)
-STKR2 = os.environ.get("STKR2", None)
-STKR3 = os.environ.get("STKR3", None)
-STKR4 = os.environ.get("STKR4", None)
-STKR5 = os.environ.get("STKR5", None)
-STKR6 = os.environ.get("STKR6", None)
-STKR7 = os.environ.get("STKR7", None)
-STKR8 = os.environ.get("STKR8", None)
-STKR9 = os.environ.get("STKR9", None)
-OWNER_ID = os.environ.get("OWNER_ID")
+START_IMG2 = os.environ.get("START_IMG2")
+START_IMG3 = os.environ.get("START_IMG3")
+START_IMG4 = os.environ.get("START_IMG4")
+START_IMG5 = os.environ.get("START_IMG5")
+START_IMG6 = os.environ.get("START_IMG6")
+START_IMG7 = os.environ.get("START_IMG7")
+START_IMG8 = os.environ.get("START_IMG8")
+START_IMG9 = os.environ.get("START_IMG9")
+START_IMG10 = os.environ.get("START_IMG10")
+STKR = os.environ.get("STKR", "CAACAgQAAxkBAALRimNZXTpB8mhQbnAAAWAvCV4Ya1uHFQACnxEAAqbxcR57wYUDyflSISoE")
+STKR1 = os.environ.get("STKR1", "CAACAgQAAxkBAALRi2NZXUgjZCT775L5Nr0XrLbQ6XIpAAK_EQACpvFxHq2xh5JRVJNrKgQ")
+STKR2 = os.environ.get("STKR2", "CAACAgQAAxkBAALRjGNZXUs6YPggISBdtg4nXaU0vjNzAALqCwACbCIRU61ZQKi3F88DKgQ")
+STKR3 = os.environ.get("STKR3", "CAACAgQAAxkBAALRjWNZXUvETcfHR2Yi9ftTQLLP2uD8AAIVDAAC1SMQU-QrCHEcbz8rKgQ")
+STKR4 = os.environ.get("STKR4", "CAACAgQAAxkBAALRjmNZXWw-WbZ_iAg-4UGixa7WSz3RAAK9CQACelwRUzpqVCTmeOrfKgQ")
+STKR5 = os.environ.get("STKR5", "CAACAgQAAxkBAALRj2NZXXJw6Pw7TJgYQStoq4u2oYpmAAKgEQACpvFxHk7lQeNrq3NMKgQ")
+STKR6 = os.environ.get("STKR6", "CAACAgQAAxkBAALRkGNZXYmAXYRR4lmCxHGPgG012Vm0AAJiFwACpvFxHuCsJc_EpuEVKgQ")
+STKR7 = os.environ.get("STKR7", "CAACAgQAAxkBAALRkWNZXYyCvkfI4d1lK0AEMkG0GdUmAAJmFwACpvFxHnvJHTM8_o9XKgQ")
+STKR8 = os.environ.get("STKR8", "CAACAgQAAxkBAALRkmNZXZg1zuakmgkPf2lfXPXi4bZaAALACgACQUGpUjAAAYL3e09XCyoE")
+STKR9 = os.environ.get("STKR9", "CAACAgQAAxkBAALRpWNZXzUmc-SfzlpERZFVXwABEluuGQACbQsAAlBzGVO38BzgGrQ1sCoE")
 
 bot = Client(
     "VickBot" ,
@@ -107,7 +106,7 @@ START = f"""
 """
 DEV_OP = [
     [
-        InlineKeyboardButton(text="🥀 ᴏᴡɴᴇʀ 🥀", user_id=f"{USER_ID}"),
+        InlineKeyboardButton(text="🥀 ᴏᴡɴᴇʀ 🥀", url=f"t.me/{OWNER_USERNAME}"),
         InlineKeyboardButton(text="✨ ꜱᴜᴘᴘᴏʀᴛ ✨", url=f"https://t.me/{SUPPORT_GRP}"),
     ],
     [
