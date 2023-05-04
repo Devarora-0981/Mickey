cacd0693b768a42a312b4e194a5b79f66af1b712
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-04 11:45:18 +0530

    Create ping.py

---
+++ Mickey/modules/ping.py
@@ -0,0 +1,39 @@
+#Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
+#Github :- Devarora-0981 | Devarora2604
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from datetime import datetime
+
+from pyrogram import Client, filters
+from pyrogram.types import *
+from config import OWNER_USERNAME
+from inline import *
+from read import *
+
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+from Mickey import App
+from Mickey.database import *
+from Mickey.modules.helpers import *
+
+
+@App.on_message(filters.command("ping", prefixes=["+", "/", "-", "?", "$", "&"]))
+async def ping(client: App, message: Message):
+    if message.chat.type == "private":
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
+    await message.delete()
+    start = datetime.now()
+    wtfbhemchomd = await message.reply_sticker(sticker= random.choice(STICKER))
+    ms = (datetime.now()-start).microseconds / 1000
+    me = await client.get_me()
+    await message.reply_photo(
+        photo=random.choice(PHOTO),
+        caption=f"нey вαву!!\n**[{me.first_name}](t.me/{me.username})** ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>||мαdє ωιтн ❣️ ву [ᴅᴇᴠᴇʟᴏᴘᴇʀ](https://t.me/{OWNER_USERNAME})||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
