9d0fc4e424018fecc44db7e07d93605903260e9c
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-04 16:26:21 +0530

    Update __main__.py

--- Mickey/__main__.py
+++ Mickey/__main__.py
@@ -1,1 +1,9 @@
+# import uvloop  # Comment it out if using on windows
+
+from Mickey import LOGGER, App
+
+if __name__ == "__main__":
+    # uvloop.install()  # Comment it out if using on windows
+    LOGGER.info("Bot Started")
+    App().run()
 
