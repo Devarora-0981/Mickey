e1e25d8ca1f31ffa8c253a4a9f363f7d2d0cdfae
Abishnoi69 <1072003+yourmail69+@github.com>
2023-05-04 06:20:53 +0000

    Auto-ғɪx

--- Mickey/modules/callback.py
+++ Mickey/modules/callback.py
@@ -1,59 +1,58 @@
+from Mickey import App, vick
 
-from Mickey import App, vick, vickdb
 
 @App.on_callback_query()
 async def cb_handler(Client: App, query: CallbackQuery):
     if query.data == "HELP":
         await query.message.edit_text(
-                      text = HELP_READ,
-                      reply_markup=InlineKeyboardMarkup(HELP_BTN),
-                      disable_web_page_preview=True,
-     )
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+            disable_web_page_preview=True,
+        )
     elif query.data == "CLOSE":
-            await query.message.delete()
+        await query.message.delete()
     elif query.data == "BACK":
-            await query.message.edit(
-                  text = START,
-                  reply_markup=InlineKeyboardMarkup(DEV_OP),
-     )
+        await query.message.edit(
+            text=START,
+            reply_markup=InlineKeyboardMarkup(DEV_OP),
+        )
     elif query.data == "SOURCE":
-            await query.message.edit(
-                   text = SOURCE_READ,
-                   reply_markup = InlineKeyboardMarkup(BACK),
-                   disable_web_page_preview = True,
-
-     )
+        await query.message.edit(
+            text=SOURCE_READ,
+            reply_markup=InlineKeyboardMarkup(BACK),
+            disable_web_page_preview=True,
+        )
     elif query.data == "ABOUT":
-            await query.message.edit(
-                    text = ABOUT_READ,
-                    reply_markup = InlineKeyboardMarkup(ABOUT_BTN),
-                    disable_web_page_preview=True,
-     )
+        await query.message.edit(
+            text=ABOUT_READ,
+            reply_markup=InlineKeyboardMarkup(ABOUT_BTN),
+            disable_web_page_preview=True,
+        )
     elif query.data == "ADMINS":
-            await query.message.edit(
-                    text = ADMIN_READ,
-                    reply_markup = InlineKeyboardMarkup(MUSIC_BACK_BTN), 
-     )
-    elif query.data== "TOOLS_DATA":
-            await query.message.edit(
-                    text= TOOLS_DATA_READ,
-                    reply_markup = InlineKeyboardMarkup(CHATBOT_BACK),
-     )
+        await query.message.edit(
+            text=ADMIN_READ,
+            reply_markup=InlineKeyboardMarkup(MUSIC_BACK_BTN),
+        )
+    elif query.data == "TOOLS_DATA":
+        await query.message.edit(
+            text=TOOLS_DATA_READ,
+            reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
+        )
     elif query.data == "BACK_HELP":
-            await query.message.edit(
-                    text = HELP_READ,
-                    reply_markup = InlineKeyboardMarkup(HELP_BTN),
-     )
+        await query.message.edit(
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+        )
     elif query.data == "CHATBOT_CMD":
-            await query.message.edit(
-                    text = CHATBOT_READ,
-                    reply_markup = InlineKeyboardMarkup(CHATBOT_BACK), 
-     )
+        await query.message.edit(
+            text=CHATBOT_READ,
+            reply_markup=InlineKeyboardMarkup(CHATBOT_BACK),
+        )
     elif query.data == "CHATBOT_BACK":
-            await query.message.edit(
-                    text = HELP_READ,
-                    reply_markup = InlineKeyboardMarkup(HELP_BTN), 
-     )
+        await query.message.edit(
+            text=HELP_READ,
+            reply_markup=InlineKeyboardMarkup(HELP_BTN),
+        )
     elif query.data == "addchat":
         if query.from_user.id not in (await is_admins(query.message.chat.id)):
             return query.answer(
@@ -62,11 +61,13 @@ async def cb_handler(Client: App, query: CallbackQuery):
             )
         else:
             is_vick = vick.find_one({"chat_id": query.message.chat.id})
-            if not is_vick:           
+            if not is_vick:
                 await query.edit_message_text(f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴀʟʀᴇᴀᴅʏ ᴇɴᴀʙʟᴇᴅ.**")
             if is_vick:
                 vick.delete_one({"chat_id": query.message.chat.id})
-                await query.edit_message_text(f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}.")
+                await query.edit_message_text(
+                    f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴇɴᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}."
+                )
     elif query.data == "rmchat":
         if query.from_user.id not in (await is_admins(query.message.chat.id)):
             return query.answer(
@@ -77,7 +78,8 @@ async def cb_handler(Client: App, query: CallbackQuery):
             is_vick = vick.find_one({"chat_id": query.message.chat.id})
             if not is_vick:
                 vick.insert_one({"chat_id": query.message.chat.id})
-                await query.edit_message_text(f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}.")
+                await query.edit_message_text(
+                    f"**ᴄʜᴀᴛ-ʙᴏᴛ ᴅɪsᴀʙʟᴇᴅ ʙʏ** {query.from_user.mention}."
+                )
             if is_vick:
                 await query.edit_message_text("**ᴄʜᴀᴛ-ʙᴏᴛ ᴀʟʀᴇᴀᴅʏ ᴅɪsᴀʙʟᴇᴅ.**")
-
