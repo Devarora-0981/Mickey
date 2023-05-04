5b518f4f629ed7b722b17f6f210717dbcf8026c3
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-04 10:38:17 +0530

    Update start.py

--- Mickey/modules/start.py
+++ Mickey/modules/start.py
@@ -1,1 +1,80 @@
 
+
+#Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
+#Github :- Devarora-0981 | Devarora2604
+
+import requests
+import random
+import os
+import re
+import asyncio
+import time
+from datetime import datetime
+from pyrogram import filters
+from pyrogram.types import *
+from config import *
+from Mickey.modules.helpers import *
+from Mickey.database import *
+from Mickey import App
+from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient as MongoCli
+
+
+
+
+PHOTO = [
+    START_IMG1,
+    START_IMG2,
+    START_IMG3,
+]
+
+EMOJIOS = [ 
+      "💣",
+      "💥",
+      "🪄",
+      "🧨",
+      "⚡",
+      "🤡",
+      "👻",
+      "🎃",
+      "🎩",
+      "🕊",
+]
+      
+STICKER = [
+      STKR1,
+      STKR2,
+      STKR3,
+]
+
+
+@App.on_message(filters.command(["start", "aistart", f"start@{BOT_USERNAME}"]))
+async def restart(client: App, m: Message):
+    if m.chat.type == "private":
+        accha = await m.reply_text(
+            text = random.choice(EMOJIOS),
+        )
+        await asyncio.sleep(1.3)
+        await accha.edit("__ᴅιиg ᴅσиg ꨄ︎ ѕтαятιиg..__")
+        await asyncio.sleep(0.2)
+        await accha.edit("__ᴅιиg ᴅσиg ꨄ sтαятιиg.....__")
+        await asyncio.sleep(0.2)
+        await accha.edit("__ᴅιиg ᴅσиg ꨄ︎ sтαятιиg..__")
+        await asyncio.sleep(0.2)
+        await accha.delete()
+        umm = await m.reply_sticker(sticker=random.choice(STICKER))
+        await asyncio.sleep(2)
+        await umm.delete()
+        await m.reply_photo(
+            photo = random.choice(PHOTO),
+            caption=f"""**๏ ʜᴇʏ, ɪ ᴀᴍ {client.me.mention}**\n**➻ ᴀɴ ᴀɪ ʙᴀsᴇᴅ ᴄʜᴀᴛʙᴏᴛ.**\n**──────────────**\n**➻ ᴜsᴀɢᴇ /chatbot [ᴏɴ/ᴏғғ]**\n<b>||๏ ʜɪᴛ ʜᴇʟᴘ ʙᴜᴛᴛᴏɴ ғᴏʀ ʜᴇʟᴘ||</b>""",
+            reply_markup=InlineKeyboardMarkup(DEV_OP),
+        )
+        await add_served_user(m.from_user.id)
+    else:
+        await m.reply_photo(
+            photo=random.choice(PHOTO),
+            caption=START,
+            reply_markup=InlineKeyboardMarkup(HELP_START),
+        )
+        await add_served_chat(m.chat.id) import App
