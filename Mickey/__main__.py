f2d7f52df4cc959e89de795daa7685fef5225c16
Abishnoi69 <1072003+yourmail69+@github.com>
2023-05-05 05:18:06 +0000

    Auto-ғɪx

--- Mickey/__main__.py
+++ Mickey/__main__.py
@@ -3,7 +3,7 @@ import importlib
 
 from pyrogram import idle
 
-from Mickey import app, LOGGER
+from Mickey import LOGGER, app
 from Mickey.modules import all_modules
 
 
@@ -14,9 +14,10 @@ async def dev_boot():
         LOGGER.error(ex)
         quit(1)
     for mod in await all_modules():
-        imported_module = importlib.import_module(f"Mickey.modules.{mod}")
+        importlib.import_module(f"Mickey.modules.{mod}")
     LOGGER.info("Mickey Chat Bot Started.")
     await idle()
+
 
 if __name__ == "__main__":
     asyncio.get_event_loop().run_until_complete(dev_boot())
