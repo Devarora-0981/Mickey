69fca0ecb582acd34b095008a7f27fad5b933845
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-06 13:52:38 +0530

    Create stats.py

---
+++ Mickey/modules/stats.py
@@ -0,0 +1,23 @@
+import asyncio
+
+from pyrogram import filters
+from pyrogram.errors import FloodWait
+from pyrogram.types import Message
+
+from Mickey import OWNER, MickeyBot
+from Mickey.database.chats import get_served_chats
+from Mickey.database.users import get_served_users
+
+
+@MickeyBot.on_message(filters.command("stats") & filters.user(OWNER))
+async def stats(cli: MickeyBot, message: Message):
+    users = len(await get_served_users())
+    chats = len(await get_served_chats())
+    await message.reply_text(
+        f"""ᴛᴏᴛᴀʟ sᴛᴀᴛs ᴏғ {(await cli.get_me()).mention} :
+
+➻ **ᴄʜᴀᴛs :** {chats}
+➻ **ᴜsᴇʀs :** {users}"""
+    )
+
+
