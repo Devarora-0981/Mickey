df5b0ba60a1debf11918e4eff088d80035f66f74
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-06 15:47:49 +0530

    Update __init__.py

--- Mickey/modules/helpers/__init__.py
+++ Mickey/modules/helpers/__init__.py
@@ -1,2 +1,22 @@
+from typing import Callable
+
+from pyrogram.enums import ChatMemberStatus
+from pyrogram.types import Message
+
+from Mickey import OWNER, MickeyBot
+
+
+def is_admins(func: Callable) -> Callable:
+    async def non_admin(c: MickeyBot, m: Message):
+        if m.from_user.id == OWNER:
+            return await func(c, m)
+
+        admin = await c.get_chat_member(m.chat.id, m.from_user.id)
+        if admin.status in [ChatMemberStatus.OWNER, ChatMemberStatus.ADMINISTRATOR]:
+            return await func(c, m)
+
+    return non_admin
+
+  
 from .inline import *
 from .read import *
