c3cad28a8228b66e17a71dc3a6ae1c44ddeafe6b
Abishnoi69 <1072003+yourmail69+@github.com>
2023-05-06 08:39:14 +0000

    Auto-ғɪx

--- Mickey/modules/helpers/inline.py
+++ Mickey/modules/helpers/inline.py
@@ -1,7 +1,7 @@
 from pyrogram.types import InlineKeyboardButton
 
 from config import SUPPORT_GRP, UPDATE_CHNL
-from Mickey import OWNER, MickeyBot, BOT_USERNAME
+from Mickey import BOT_USERNAME, OWNER
 
 DEV_OP = [
     [
