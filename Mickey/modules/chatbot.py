1e818c5657b945a5d5b9e9714044de9c6d71b84e
Abishnoi69 <1072003+yourmail69+@github.com>
2023-05-06 10:16:21 +0000

    Auto-ғɪx

--- Mickey/modules/chatbot.py
+++ Mickey/modules/chatbot.py
@@ -9,22 +9,6 @@ from pyrogram.types import Message
 
 from config import MONGO_URL
 from Mickey import MickeyBot
-from Mickey.modules.helpers import (
-    ABOUT_BTN,
-    ABOUT_READ,
-    ADMIN_READ,
-    BACK,
-    CHATBOT_BACK,
-    CHATBOT_READ,
-    DEV_OP,
-    HELP_BTN,
-    HELP_READ,
-    MUSIC_BACK_BTN,
-    SOURCE_READ,
-    START,
-    TOOLS_DATA_READ,
-)
-
 
 
 @MickeyBot.on_message(filters.command(["chatbot"]) & ~filters.private)
