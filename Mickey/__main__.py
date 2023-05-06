392bd5feebd19a1163242fba1ca85bd18b895c6b
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-06 10:26:27 +0530

    Update __main__.py

--- Mickey/__main__.py
+++ Mickey/__main__.py
@@ -4,17 +4,20 @@ import importlib
 from pyrogram import idle
 
 from Mickey import LOGGER, app
-from Mickey.modules import all_modules
+# from Mickey.modules import all_modules
 
-
+"""
 async def dev_boot():
     await app.start()
     for mod in await all_modules():
         importlib.import_module(f"Mickey.modules.{mod}")
     LOGGER.info("Mickey Chat Bot Started.")
     await idle()
+"""
 
 
+    
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(dev_boot())
-    LOGGER.info("Stopping Mickey Chat Bot...")
+   # asyncio.get_event_loop().run_until_complete(dev_boot())
+    
+    app().run()
