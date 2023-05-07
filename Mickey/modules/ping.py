f6a0856027e04d5d41d8ee1546bf3f9aca92f36b
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-05-07 11:21:25 +0530

    Create ping.py

---
+++ Mickey/modules/ping.py
@@ -0,0 +1,38 @@
+# Don't remove This Line From Here. Tg: @Dev_Arora_0981 | @DevArora0981
+# Github :- Devarora-0981 | Devarora2604
+
+import random
+from datetime import datetime
+
+from pyrogram import filters
+from pyrogram.enums import ChatType
+from pyrogram.types import InlineKeyboardMarkup, Message
+
+from config import IMG, STICKER, OWNER_USERNAME
+from Mickey import dev, BOT_NAME
+from Mickey.database.chats import add_served_chat
+from Mickey.database.users import add_served_user
+from Mickey.modules.helpers import PNG_BTN
+
+
+@dev.on_message(filters.command("ping", prefixes=["+", "/", "-", "?", "$", "&"]))
+async def ping(_, message: Message):
+    start = datetime.now()
+    loda = await message.reply_photo(
+        photo=random.choice(IMG),
+        caption="ᴘɪɴɢɪɴɢ...",
+    )
+    try:
+        await message.delete()
+    except:
+        pass
+    await message.reply_sticker(sticker=random.choice(STICKER))
+    ms = (datetime.now() - start).microseconds / 1000
+    await loda.edit_text(
+        text=f"нey вαву!!\n{BOT_NAME} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅᴇᴠ](https://t.me/{OWNER_USERNAME}) ||</b>",
+        reply_markup=InlineKeyboardMarkup(PNG_BTN),
+    )
+    if message.chat.type == ChatType.PRIVATE:
+        await add_served_user(message.from_user.id)
+    else:
+        await add_served_chat(message.chat.id)
