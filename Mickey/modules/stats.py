ee8fc258152f9237e0ca371b47cd21426d163b70
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-11 10:28:00 +0530

    Update stats.py

--- Mickey/modules/stats.py
+++ Mickey/modules/stats.py
@@ -1,4 +1,4 @@
-from pyrogram import filters
+from pyrogram import filters, Client
 from pyrogram.types import Message
 
 from Mickey import OWNER, MickeyBot
@@ -7,7 +7,7 @@ from Mickey.database.users import get_served_users
 
 
 @MickeyBot.on_message(filters.command("stats") & filters.user(OWNER))
-async def stats(cli: dev, message: Message):
+async def stats(cli: Client, message: Message):
     users = len(await get_served_users())
     chats = len(await get_served_chats())
     await message.reply_text(
