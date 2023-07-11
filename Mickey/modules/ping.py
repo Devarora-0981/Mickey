f4e8b248cfa8a4dd99c464329f39d2de884a6662
ᴍʀ. x <114943948+Abishnoi69@users.noreply.github.com>
2023-07-11 10:03:49 +0530

    Update ping.py

--- Mickey/modules/ping.py
+++ Mickey/modules/ping.py
@@ -9,13 +9,13 @@ from pyrogram.enums import ChatType
 from pyrogram.types import InlineKeyboardMarkup, Message
 
 from config import IMG, OWNER_USERNAME, STICKER
-from Mickey import BOT_NAME, dev
+from Mickey import MickeyBot
 from Mickey.database.chats import add_served_chat
 from Mickey.database.users import add_served_user
 from Mickey.modules.helpers import PNG_BTN
 
 
-@dev.on_message(filters.command("ping", prefixes=["+", "/", "-", "?", "$", "&"]))
+@MickeyBot.on_cmd("ping")
 async def ping(_, message: Message):
     await message.reply_sticker(sticker=random.choice(STICKER))
     start = datetime.now()
@@ -30,7 +30,7 @@ async def ping(_, message: Message):
 
     ms = (datetime.now() - start).microseconds / 1000
     await loda.edit_text(
-        text=f"нey вαву!!\n{BOT_NAME} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅᴇᴠ](https://t.me/{OWNER_USERNAME}) ||</b>",
+        text=f"нey вαву!!\n{MickeyBot.name} ιѕ alιve 🥀 αnd worĸιng ғιne wιтн a pιng oғ\n➥ `{ms}` ms\n\n<b>|| мαdє ωιтн ❣️ ву [ᴅᴇᴠ](https://t.me/{OWNER_USERNAME}) ||</b>",
         reply_markup=InlineKeyboardMarkup(PNG_BTN),
     )
     if message.chat.type == ChatType.PRIVATE:
